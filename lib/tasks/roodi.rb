bbe90cd22b9cd3c810103ddfa0d0232785cfef84
Duncan Bayne <duncan@bayne.id.au>
2016-05-04 15:20:42 +1000

    Remove everything except README

--- lib/tasks/roodi.rb
+++
@@ -1,8 +0,0 @@
-require 'roodi'
-
-def roodi(ruby_files)
-  roodi = Roodi::Core::Runner.new
-  ruby_files.each { |file| roodi.check_file(file) }
-  roodi.errors.each {|error| puts error}
-  puts "\nFound #{roodi.errors.size} errors."
-end
